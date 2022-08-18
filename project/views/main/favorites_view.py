660cce6823a0682cfb6c58bcef7fe55544fc48d9
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-08-18 14:27:06 +0300

    some little additions

--- project/views/main/favorites_view.py
+++ project/views/main/favorites_view.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource, Namespace
 
 from project.container import favorite_service, user_service
-from project.setup.api.models import error
+from project.setup.api.models import error, favorite_model
 from project.tools.decorators import auth_required
 
 favorites_ns: Namespace = Namespace('favorites/movies', description='namespace for favorites')
@@ -12,13 +12,14 @@ class FavoritesView(Resource):
     @staticmethod
     @favorites_ns.response(404, 'Not found', error)
     @favorites_ns.response(401, 'Not authorized', error)
+    @favorites_ns.marshal_with(favorite_model, code=200, description='OK')
     @auth_required
     def post(username, movie_id: int):
         user: dict = user_service.get_user_by_email_and_password(email=username)
         user_id: int = user['id']
 
-        favorite_service.post_new_favorite(user_id, movie_id)
-        return "", 201
+        new_favorite = favorite_service.post_new_favorite(user_id, movie_id)
+        return new_favorite, 201
 
     @staticmethod
     @favorites_ns.response(404, 'Not found', error)
