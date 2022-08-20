1d2b262879ba98bd82c23c4a1effabadff64fbba
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-08-20 14:42:23 +0300

    changed session type to global, removed scoped_session from dao init

--- project/container.py
+++ project/container.py
@@ -1,20 +1,20 @@
 from project.dao import MovieDAO, GenreDAO, DirectorDAO, UserDAO, FavoriteDAO
 from project.services import MovieService, GenreService, DirectorService, UserService, AuthService, FavoriteService
-from project.setup.db import db
+
 # movies
-movie_dao = MovieDAO(db_session=db.session)
+movie_dao = MovieDAO()
 movie_service = MovieService(dao=movie_dao)
 # genres
-genre_dao = GenreDAO(db_session=db.session)
+genre_dao = GenreDAO()
 genre_service = GenreService(dao=genre_dao)
 # directors
-director_dao = DirectorDAO(db_session=db.session)
+director_dao = DirectorDAO()
 director_service = DirectorService(dao=director_dao)
 # users
-user_dao = UserDAO(db_session=db.session)
+user_dao = UserDAO()
 user_service = UserService(dao=user_dao)
 # authorization
 auth_service = AuthService(user_service=user_service)
 # favorites
-favorite_dao = FavoriteDAO(db_session=db.session)
+favorite_dao = FavoriteDAO()
 favorite_service = FavoriteService(dao=favorite_dao)
